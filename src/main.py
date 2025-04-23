1889491758e4c03c71892e3c28b0b76d9ee490ce
Zolkyed <zolkyed@gmail.com>
2025-04-23 01:34:41 -0400

    add: Dockerfile

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from parse import fetch_books_data, fetch_yahoo_finance
 from save_csv import save_data_to_csv
-from tqdm import tqdm 
+from tqdm import tqdm
 
 def main():
-  books = fetch_books_data()
-  stocks = fetch_yahoo_finance()
-
-  save_data_to_csv(books, columns=['Title', 'Price', 'Star Rating'], filename='books.csv')
-  save_data_to_csv(stocks, columns=['Name', 'Symbol', 'Price'], filename='stocks.csv')
+    books = fetch_books_data()
+    books_with_progress = tqdm(books, desc="Fetching books", unit="book")
+    
+    stocks = fetch_yahoo_finance()
+    stocks_with_progress = tqdm(stocks, desc="Fetching stocks", unit="stock")
+    
+    save_data_to_csv(books_with_progress, columns=['Title', 'Price', 'Star Rating'], filename='books.csv')
+    save_data_to_csv(stocks_with_progress, columns=['Name', 'Symbol', 'Price'], filename='stocks.csv')
 
 if __name__ == "__main__":
-    main() 
+    main()
